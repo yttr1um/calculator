e5de55dcb98143b1ad38e93711b16f053e393053
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-04-09 19:47:06 +0400

    Add calculator mechanics (calculating, erasing)
    add the ability to calculate using +, -, *, / and be able to erase.

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
-let numberBtns = document.querySelectorAll(".number");
-let operationBtns = document.querySelectorAll(".operation");
-let cancelBtn = document.querySelector("#cancel");
 let screen = document.querySelector("#calc-screen");
+let buttons = document.querySelectorAll("button");
+let existingOperation =  false;
+let operations = ['+', '-', '*', '/'];
 
-numberBtns.forEach(numberBtn => {
-    numberBtn?.addEventListener("click", () => {
-        let number = document.createElement("p");
-        number.textContent = numberBtn.value;
-        screen.appendChild(number);
+function calculate(problem) {
+    
+    //get the operation used in the problem.
+    let operationUsed;
+    operations.forEach(operation => {
+        if (problem.includes(operation)) {
+            operationUsed = operation;
+        }
     })
-})
 
-cancelBtn.addEventListener("click", () => {
-    for (let text of Array.from(screen.children)) {
-        text.remove();
+    //get the 2 numbers in the problem.
+    let numbers = problem.split(operationUsed);
+
+    //calculate!!!!
+    switch(operationUsed) {
+        case "+":
+            return +numbers[0] + +numbers[1];
+            break;
+
+        case "-":
+            return +numbers[0] - +numbers[1];
+            break;
+
+        case "*":
+            return +numbers[0] * +numbers[1];
+            break;
+
+        case "/":
+            return +numbers[0] / +numbers[1];
+            break;
     }
-})
+}
 
-operationBtns.forEach(operationBtn => {
-    operationBtn?.addEventListener("click", () => {
-        let operation = document.createElement("p");
-        operation.textContent = "+";
-        screen.appendChild(operation);
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        let target = event.target;
+
+        switch(target.className) {
+
+            case "number":
+                let number = document.createElement("p");
+                number.textContent = target.value;
+                screen.appendChild(number);
+                break;
+
+            case "operation":
+                if (!existingOperation) {
+                    let operation = document.createElement("p");
+                    operation.textContent = target.value;
+                    firstNum = screen.textContent;
+                    screen.appendChild(operation);
+                    existingOperation = true;
+                }
+                break;
+
+            case "cancel":
+                for (let text of Array.from(screen.children)) {
+                    text.remove();
+                }
+                existingOperation = false;
+                break;
+
+            case "equal":
+                let problem = screen.textContent;
+                
+                let result = document.createElement("p");
+                result.textContent = calculate(problem);
+
+                //clear the screen.
+                for (let text of Array.from(screen.children)) {
+                    text.remove();
+                }
+                existingOperation = false;
+
+                screen.appendChild(result);
+                break;
+
+        }
     })
 })
